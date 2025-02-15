1e96e2dbe83c8392be7b642ace4d658357f69dbc
Forkxel <pavel.halik06@gmail.com>
2025-02-15 17:12:12 +0100

    Created class for Category

---
+++ Database project/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_project
+{
+    public class Category
+    {
+        private int id;
+        private string name;
+
+        public int ID { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Category(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+    }
+}
